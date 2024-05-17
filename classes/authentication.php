04502e6177b7ba3959ef93d2abe7557e4e6e3ae5
Mahmoud nayel <74990836+dedo245@users.noreply.github.com>
2024-05-18 00:53:16 +0300

    f

--- classes/authentication.php
+++ classes/authentication.php
@@ -3,30 +3,61 @@ namespace App;
 
 require'classes\connect.php';
 
-class authentication{
-    function admin()
+
+class Authentication
+{
+    
+    public function auth()
     {
-        global $conn;
-        if (isset($_POST['signIn'])) {
-            $email = ($_POST['email'] === 'admin@gmail.com');
-            $password = $_POST['password'];
-            $password = md5($password);
-
-            $sql = "SELECT * FROM users";
-            $result = $conn->query($sql);
-            if ($result->num_rows > 0) {
-                session_start();
-
-                $row = $result->fetch_assoc();
-                $_SESSION['email'] = $row['email'];
-                header("Location: order.php");
-                exit();
-            } else {
-                echo "Not Found, Incorrect Email or Password";
-            }
-
+        if (!isset($_SESSION['user_id'])) {
+            header('Location: ../homepage.php');
         }
     }
+
+    // return true or false
+    public function is_auth()
+    {
+        return isset($_SESSION['user_id']);
+    }
+
+    public function logOut()
+    {
+        // check if key ?logout= exists in URL
+        if (isset($_GET['logout'])) {
+            session_unset();
+            session_destroy();
+            header('Location: ../homepage.php');
+        }
+    }
+
+
+        
+    
+    public function signin()
+        
+        {
+            if(isset($_POST['signIn'])){
+                require 'classes\connect.php';
+                $email=$_POST['email'];
+                $password=$_POST['password'];
+                $password=md5($password) ;
+            
+                $sql="SELECT * FROM users WHERE email='$email' and password='$password'";
+                $result=$conn->query($sql);
+            if($result->num_rows==1){
+                session_start();
+            
+                $row=$result->fetch_assoc();
+                $_SESSION['email']=$row['email'];
+                $_SESSION['user_id']=$row['id'];
+                header("Location: ../order.php");
+                exit();
+            }
+            else{
+                echo "Not Found, Incorrect Email or Password";
+            } }
+        
+        }
 }
 ?>
 
