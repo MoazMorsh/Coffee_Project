e34c3bd669137ff62bd3d82c8b2f10aa78eceb37
Mahmoud Nayel <74990836+MahmoudNayel24@users.noreply.github.com>
2024-05-17 20:48:29 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -6,18 +6,6 @@ header("location: homepage.php");
 
 
 
-//ne3ml logout function
-//user wahed nesameh admin yeshof kolo
-//momken nsplit kol page signin w up kol html lwahdo w php yeb2a metgama3 f heta
-//kan 3awz ye3ml check low email mwgod bs password ghalat ye2ol pass ghalt low user msh mwgod asl ye2ol not found
-
-
-
-
-
-/*haktb variable t call el namespace/authentication
-/*ha call el function b el variable dah $variable->function_name
-
 
 
 
